eca82b44b9da91f44a876ad2e9f77823825bafc6
SahanJayasinghe <47143531+SahanJayasinghe@users.noreply.github.com>
2020-07-15 19:43:01 +0530

    env var reset

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -4,4 +4,4 @@
 
 // export const name_pattern = '^(?=.*[A-Za-z])[A-Za-z\-,.\s]{1,50}$';
 
-export const server_baseURL = 'https://post-office-mis-server.herokuapp.com';
+export const server_baseURL = (process.env.REACT_APP_PO_MIS_SERVER) ? process.env.REACT_APP_PO_MIS_SERVER : 'http://localhost:5000';
